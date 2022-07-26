6ecea6e0975b2925436336f97e0dbbeb94f6dc58
wahyuindra <wahyuindra712@gmail.com>
2022-07-26 11:44:51 +0700

    add constant

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: public_member_api_docs
 
-const kBaseUrl = 'https://templates.transtrack.id/api';
+const kBaseUrl = 'https://live.transtrack.id/api/v1';
 
 const kAccessToken = 'access_token';
 
+const kPermissions = {};
+
+const kNavbarHeight = 72.0;
+
 class ApiEndPoint {
-  static const kApiExample = '/v1/example';
+  static const kApiLogin = '/login';
 }
+
